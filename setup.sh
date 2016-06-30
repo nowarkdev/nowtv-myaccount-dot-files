7a5053016f4f36a1bd0c2d536f2a3337e969941f
Noah Ark <nowarkdev@gmail.com>
2016-06-30 12:42:53 +0100

    AF: Download iTerm and vim Pencil theme in setup script

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Vim
 ln -s ${BASEDIR}/vimrc ~/.vimrc
 
-# tmux
+# Tmux
 ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
+
+# Download iTerm Pencil Dark Theme
+wget https://raw.githubusercontent.com/mattly/iterm-colors-pencil/master/pencil-dark.itermcolors
+
+# Download Pencil Vim colours 
+wget https://raw.githubusercontent.com/reedes/vim-colors-pencil/master/colors/pencil.vim -P ~/.vim/colors/
